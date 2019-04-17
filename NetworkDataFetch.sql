7f0065445ecaed3224c5e00819b46edf73d33c53
lemonci <hongyanchangfei@gmail.com>
2019-04-17 09:08:50 +0800

    Update NetworkDataFetch.sql

--- NetworkDataFetch.sql
+++ NetworkDataFetch.sql
@@ -17,3 +17,12 @@ SELECT c_personid AS personA, c_kin_id AS personB, c_kin_code from kin_data
         SELECT c_personid FROM biog_main WHERE c_index_year <= 1912 AND c_index_year >= 618)
     AND
     c_kin_code IN (75, 82, 107, 111, 135, 138, 163, 168, 176, 180, 182, 183, 184, 185, 186, 188, 191, 193, 194, 195, 196, 198, 199, 202, 204, 205, 206, 207, 211, 212, 213, 220, 221, 222, 226, 227, 229, 230, 231, 234, 235, 307, 326, 327, 329, 334, 335, 339, 343, 344, 363, 373, 377, 378, 420, 436, 437, 439, 440, 450, 451, 452, 453, 454, 455, 456, 457, 458, 467, 468, 559, 560, 575);
+
+/* Get office ID */
+SELECT posted_to_office_data.c_office_id, COUNT(posted_to_office_data.c_office_id),
+office_codes.c_office_chn, office_codes.c_dy
+FROM posted_to_office_data LEFT JOIN office_codes on posted_to_office_data.c_office_id = office_codes.c_office_id
+WHERE posted_to_office_data.c_office_id in (SELECT DISTINCT posted_to_office_data.c_office_id
+FROM posted_to_office_data INNER JOIN biog_main ON posted_to_office_data.c_personid = biog_main.c_personid
+WHERE c_index_year <= 1912 AND c_index_year >= 618 AND c_female = 0)
+GROUP BY posted_to_office_data.c_office_id
